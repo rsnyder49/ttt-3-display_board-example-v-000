6ea8e2f9cbd0f2dd6e6f848f84b1a76e5fc7bdf1
Robert Snyder <r.snyder49@gmail.com>
2018-07-17 19:49:21 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,7 +9,13 @@ def display_board
   puts "   |   |   "
 end         
 
-
+def display_board
+  print "   |   |   " 
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end 
 
 
    
